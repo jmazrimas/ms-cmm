26cd78ebe94d1d5106aacb6f001b971ba8673a50
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-06-27 10:37:42 -0500

    add basic file mgmt for unzipping, upload, download

---
+++ filemanagement.py
@@ -0,0 +1,55 @@
+import urllib.request
+import zipfile
+import os
+import json
+
+def download_tdp_data(url, filename):
+        urllib.request.urlretrieve(url, filename)
+        print("Data Downloaded")
+
+def unzip_directories():
+
+    directory = os.fsencode('./')
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".zip"):
+            zip_ref = zipfile.ZipFile(filename, 'r')
+            if filename.startswith("TDP"):
+                zip_ref.extractall('./TDPdata')
+            else:
+                zip_ref.extractall('./')
+            zip_ref.close()
+            continue
+        else:
+            continue
+
+def upload_report():
+    import time
+    timestamp = int(time.time())
+
+    with open('credentials.json') as json_data:
+        d = json.load(json_data)
+        access_key = d['accessKeyId']
+        secret_key = d['secretAccessKey']
+
+    from boto.s3.connection import S3Connection
+    conn = S3Connection(access_key, secret_key)
+
+    # NEED NEW BUCKET
+    bucket = conn.get_bucket('psubucket01')
+
+    from boto.s3.key import Key
+    k = Key(bucket)
+    file_name = str(timestamp)+'report.pdf'
+    k.key = file_name
+    k.set_contents_from_filename('./report.pdf')
+
+    signed_url = conn.generate_url(
+           expires_in=1814400,
+           method='GET',
+           bucket='psubucket01',
+           key=k.key,
+           query_auth=True
+       )
+
+    return signed_url
